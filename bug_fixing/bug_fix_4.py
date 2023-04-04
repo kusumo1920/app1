20da90d68300a9ae7630156ba0a42a729c5f0e39
Kesit Budi Kusumo <kesit.kusumo@shopee.com>
2023-04-04 23:33:34 +0800

    finish day 4

---
+++ bug_fixing/bug_fix_4.py
@@ -0,0 +1,9 @@
+# 1
+# elements = ['a', 'b', 'c']
+# print(elements[1])
+
+# 2
+# elements = ['a', 'b', 'c']
+# new = 'x'
+# elements[1] = new
+# print(elements)
---
+++ exercises/coding_exercises_4.py
@@ -0,0 +1,15 @@
+# 1
+#amount = int(input("How many dollars have you got? "))
+# print("The amount in euros is:")
+# usd_eur_rate = 0.95
+# print(amount * usd_eur_rate)
+
+# 2
+# ranking = ['John', 'Sen', 'Lisa']
+# user_input = int(input("Enter rank number: ")) - 1
+# print(ranking[user_input])
+
+# 3
+# ranking = ['John', 'Sen', 'Lisa']
+# user_input = input("Enter a name: ")
+# print(ranking.index(user_input) + 1)
