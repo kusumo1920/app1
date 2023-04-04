74f15bf105a2ca775cdf64db51d8ddffb441d233
Kesit Budi Kusumo <kesit.kusumo@shopee.com>
2023-04-05 02:58:29 +0800

    finish day 5

---
+++ exercises/coding_exercises_5.py
@@ -0,0 +1,9 @@
+# 1
+# filenames = ['document', 'report', 'presentation']
+# for idx, filename in enumerate(filenames):
+#     print(f"{idx}-{filename.capitalize()}.txt")
+
+# 2
+# ips = ['100.122.133.105', '100.122.133.111']
+# user_input = int(input("Enter the index of the IP you want: "))
+# print("You chose", ips[user_input])
