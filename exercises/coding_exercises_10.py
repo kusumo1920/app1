840cd1f5afd798ace3c2317e89ecf931eb4a5b2d
Kesit Budi Kusumo <kusumo1920@icloud.com>
2023-04-07 20:57:09 +0700

    finish day 10

---
+++ exercises/coding_exercises_10.py
@@ -0,0 +1,19 @@
+# 1
+# try:
+#     total_value = float(input("Enter total value: "))
+#     value = float(input("Enter value: "))
+#     percentage_value = value / total_value * 100
+#     print(f"That is {percentage_value}%")
+# except ValueError:
+#     print("You need to enter a number. Run the program again.")
+
+# 2
+# try:
+#     total_value = float(input("Enter total value: "))
+#     value = float(input("Enter value: "))
+#     percentage_value = value / total_value * 100
+#     print(f"That is {percentage_value}%")
+# except ValueError:
+#     print("You need to enter a number. Run the program again.")
+# except ZeroDivisionError:
+#     print("Your total value cannot be zero")
--- main.py
+++ main.py
@@ -2,17 +2,17 @@ while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
 
         with open("files/subfiles/todos.txt", "r") as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         with open("files/subfiles/todos.txt", "w") as file:
             file.writelines(todos)
-    elif 'show' in user_action:
+    elif user_action.startswith("show"):
         with open("files/subfiles/todos.txt", "r") as file:
             todos = file.readlines()
 
@@ -20,32 +20,39 @@ while True:
             item = item.strip("\n")
             row = f"{index + 1}-{item}"
             print(row)
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
-        number = number - 1
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
 
-        with open("files/subfiles/todos.txt", "r") as file:
-            todos = file.readlines()
+            with open("files/subfiles/todos.txt", "r") as file:
+                todos = file.readlines()
 
-        new_todo = input("Enter new todo: ")
-        todos[number] = new_todo + "\n"
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + "\n"
 
-        with open("files/subfiles/todos.txt", "w") as file:
-            file.writelines(todos)
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
+            with open("files/subfiles/todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is invalid!")
+            continue
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
 
-        with open("files/subfiles/todos.txt", "r") as file:
-            todos = file.readlines()
+            with open("files/subfiles/todos.txt", "r") as file:
+                todos = file.readlines()
 
-        removed_todo = todos.pop(number - 1).strip("\n")
+            removed_todo = todos.pop(number - 1).strip("\n")
 
-        with open("files/subfiles/todos.txt", "w") as file:
-            file.writelines(todos)
+            with open("files/subfiles/todos.txt", "w") as file:
+                file.writelines(todos)
 
-        message = f"Todo {removed_todo} was removed from the list."
-        print(message)
-    elif 'exit' in user_action:
+            message = f"Todo {removed_todo} was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that numeric id.")
+    elif user_action.startswith("exit"):
         break
     else:
         print("Command is invalid!")
