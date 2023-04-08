37bcfdd7264dfb8a3ebebfc5e6adc5183625cc22
Kesit Budi Kusumo <kusumo1920@icloud.com>
2023-04-08 19:57:38 +0700

    finish day 13

---
+++ exercises/coding_exercises_13.py
@@ -0,0 +1,28 @@
+# 1
+# def calculate_age(year_of_birth, current_year=2023):
+#     return current_year - year_of_birth
+
+# 2
+# def calculate_age(year_of_birth, current_year=2023):
+#     return current_year - year_of_birth
+#
+#
+# birth_year = int(input("What is your year of birth? "))
+# print(calculate_age(birth_year))
+
+# 3
+# def calculate_age(year_of_birth, current_year=2023):
+#     return current_year - year_of_birth
+#
+#
+# birth_year = int(input("What is your year of birth? "))
+# age = calculate_age(birth_year)
+# if age > 120:
+#     print("You're a legend!")
+# else:
+#     print("You're ordinary.")
+
+# 4
+# names = input("Enter names separated by commas (no spaces): ")
+# names = names.split(",")
+# print(len(names))
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def get_todos(filepath):
+def get_todos(filepath="files/subfiles/todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="files/subfiles/todos.txt"):
+    """ Write the to-do items list in the text file. """
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
 
@@ -16,13 +20,13 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos("files/subfiles/todos.txt")
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos("files/subfiles/todos.txt", todos)
+        write_todos(todos)
     elif user_action.startswith("show"):
-        todos = get_todos("files/subfiles/todos.txt")
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -33,12 +37,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos("files/subfiles/todos.txt")
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            write_todos("files/subfiles/todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is invalid!")
             continue
@@ -46,11 +50,11 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos("files/subfiles/todos.txt")
+            todos = get_todos()
 
             removed_todo = todos.pop(number - 1).strip("\n")
 
-            write_todos("files/subfiles/todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {removed_todo} was removed from the list."
             print(message)
