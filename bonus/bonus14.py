c1fb612daef4198a8f5c6b730980390f9b7083bd
Kesit Budi Kusumo <kusumo1920@icloud.com>
2023-04-09 03:13:05 +0700

    finish day 14

---
+++ bonus/bonus14.py
@@ -0,0 +1,12 @@
+from bonus.converters14 import convert
+from bonus.parsers14 import parse
+
+feet_inches = input("Enter feet and inches: ")
+
+parsed = parse(feet_inches)
+result = convert(parsed["feet"], parsed["inches"])
+
+if result < 1:
+    print("Kid is too small.")
+else:
+    print("Kid can use the slide.")
---
+++ exercises/coding_exercises_14.py
@@ -0,0 +1,6 @@
+# 1
+from module14 import get_state
+
+if __name__ == "__main__":
+    temperature = int(input("Enter water temperature: "))
+    print(get_state(temperature))
