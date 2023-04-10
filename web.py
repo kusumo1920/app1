a0466820ed90e7ebe10a8168d1b45c6d34820b14
Kesit Budi Kusumo <kusumo1920@icloud.com>
2023-04-10 07:01:20 +0700

    partial day 19

---
+++ web.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+st.checkbox("Buy grocery.")
+st.checkbox("Throw the trash.")
