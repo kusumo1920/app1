bc4eaefb7e87520740a6a41527f949d3d42be8d1
Kesit Budi Kusumo <kesit.kusumo@shopee.com>
2023-04-12 09:43:08 +0800

    finish day 20

---
+++ exercises/coding_exercises_19.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
+with st.expander("Start camera"):
+    camera_image = st.camera_input("Camera")
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_camera_img = img.convert('L')
+    st.image(gray_camera_img)
